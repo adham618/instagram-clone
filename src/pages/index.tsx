a7ed265b81249c58540150c0ea8e93ef514be287
adham618 <adhamtarek291@gmail.com>
2022-01-04 22:18:03 +0200

    add the components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Feed from '../common/components/Feed/Feed'
+import Header from '../common/components/Header/Header'
+import Modal from '../common/components/Modal/Modal'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <>
       <Head>
         <title>Instagram</title>
         <meta name="description" content="Instagram-clone by Adham Tarek" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-    </div>
+      <Header />
+      <Feed />
+      <Modal />
+    </>
   )
 }
 
