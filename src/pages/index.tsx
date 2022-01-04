e8dc5a33297fe8b8d8c543aff0a97234a16bad09
adham618 <adhamtarek291@gmail.com>
2022-01-04 11:58:11 +0200

    starter project with tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram</title>
+        <meta name="description" content="Instagram-clone by Adham Tarek" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
+
+export default Home
