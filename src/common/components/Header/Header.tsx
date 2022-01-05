c8dc64aacb117e8b5854dae8bcb443d7e8973a80
adham618 <adhamtarek291@gmail.com>
2022-01-05 18:30:32 +0200

    complete the riight section of the Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { SearchIcon } from '@heroicons/react/outline'
+import { HeartIcon, HomeIcon, MenuIcon, PaperAirplaneIcon, SearchIcon, UserGroupIcon } from '@heroicons/react/outline'
 
 interface HeaderProps {
 
@@ -7,13 +7,13 @@ interface HeaderProps {
 
 const Header = ({ }: HeaderProps) => {
   return (
-    <header>
+    <header className="shadow-sm border-b sticky top-0 bg-white z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* Left -Logo */}
         <div className="relative w-24 hidden lg:inline-grid cursor-pointer">
-          <Image src="/insta.svg" layout="fill" objectFit="contain" alt="logo" draggable="false" />
+          <Image src="/insta.svg" layout="fill" priority objectFit="contain" alt="logo" draggable="false" />
         </div>
-        <div className="relative w-10 lg:hidden cursor-pointer">
+        <div className="relative w-8 lg:hidden cursor-pointer">
           <Image src="/insta-logo.svg" layout="fill" priority objectFit="contain" alt="logo" draggable="false" />
         </div>
         {/* Middle - Search input field */}
@@ -23,12 +23,25 @@ const Header = ({ }: HeaderProps) => {
               <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>
             <input
-              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              className="bg-gray-50 block h-9 w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
               type="text"
               placeholder="Search" />
           </div>
         </div>
         {/* Right - Icons && Avatar */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="rotate-45 " />
+            <div className="absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+          </div>
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <div className="relative w-8 h-10">
+            <Image className="rounded-full cursor-pointer" layout="fill" priority objectFit="contain" src={"/photo.jpg"} alt="profile-pic" draggable="false" />
+          </div>
+        </div>
       </div>
     </header>
   )
