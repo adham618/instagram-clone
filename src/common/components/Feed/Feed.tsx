d003fbeedb94c1f4ad31aafc7c3457186f972c65
adham618 <adhamtarek291@gmail.com>
2022-01-06 15:02:24 +0200

    add Stories Component

--- src/common/components/Feed/Feed.tsx
+++ src/common/components/Feed/Feed.tsx
@@ -1,12 +1,21 @@
+import Stories from "./Stories"
+
 interface FeedProps {
 
 }
 
 const Feed = ({ }: FeedProps) => {
   return (
-    <div>
+    <main className="grid grid-cols-1 m-auto md:grid-cols-2 md:max-w-3xl xl-grid-cols-3 xl:max-w-6xl">
+      <section className="col-span-2">
+        <Stories />
+      </section>
+      {/* Posts */}
 
-    </div>
+      <section></section>
+      {/* MiniProfile */}
+      {/* Suggestions */}
+    </main>
   )
 }
 export default Feed
