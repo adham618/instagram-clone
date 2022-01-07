4d9f7585a2ca2b1b8d1544933030277c8bcc466d
adham618 <adhamtarek291@gmail.com>
2022-01-07 14:07:56 +0200

    create fake data (faker.js doesn't work)

---
+++ src/common/components/Feed/stories/data.tsx
@@ -0,0 +1,101 @@
+
+
+const data =
+  [
+    {
+      id: "1",
+      name: 'Vladimir Oblast',
+      avatar: '/assets/1.jpg',
+    },
+    {
+      id: "2",
+      name: 'Jodi Duke',
+      avatar: '/assets/2.jpg',
+    },
+    {
+      id: "3",
+      name: 'Leila Wilkes',
+      avatar: '/assets/3.jpg',
+    },
+    {
+      id: "4",
+      name: 'Erica Sparks',
+      avatar: '/assets/4.jpg',
+    },
+    {
+      id: "5",
+      name: 'Erica Soto',
+      avatar: '/assets/5.jpg',
+    },
+    {
+      id: "6",
+      name: 'Lina Vinson',
+      avatar: '/assets/6.jpg',
+    },
+    {
+      id: "7",
+      name: 'Jodi Kennedy',
+      avatar: '/assets/7.jpg',
+    },
+    {
+      id: "8",
+      name: 'Vladimir Oblast',
+      avatar: '/assets/8.jpg',
+    },
+    {
+      id: "9",
+      name: 'Shea Roman',
+      avatar: '/assets/9.jpg',
+    },
+    {
+      id: "10",
+      name: 'Nichole Richmond',
+      avatar: '/assets/10.jpg',
+    },
+    {
+      id: "11",
+      name: 'Jodi Duke',
+      avatar: '/assets/2.jpg',
+    },
+    {
+      id: "12",
+      name: 'Leila Wilkes',
+      avatar: '/assets/3.jpg',
+    },
+    {
+      id: "13",
+      name: 'Erica Sparks',
+      avatar: '/assets/4.jpg',
+    },
+    {
+      id: "14",
+      name: 'Erica Soto',
+      avatar: '/assets/5.jpg',
+    },
+    {
+      id: "15",
+      name: 'Lina Vinson',
+      avatar: '/assets/6.jpg',
+    },
+    {
+      id: "16",
+      name: 'Jodi Kennedy',
+      avatar: '/assets/7.jpg',
+    },
+    {
+      id: "17",
+      name: 'Vladimir Oblast',
+      avatar: '/assets/8.jpg',
+    },
+    {
+      id: "18",
+      name: 'Shea Roman',
+      avatar: '/assets/9.jpg',
+    },
+    {
+      id: "19",
+      name: 'Nichole Richmond',
+      avatar: '/assets/10.jpg',
+    },
+  ]
+export default data
