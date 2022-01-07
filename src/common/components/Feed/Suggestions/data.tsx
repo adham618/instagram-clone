ef04139476bc1d50b0313f8382a5fdb155b52d44
adham618 <adhamtarek291@gmail.com>
2022-01-07 14:07:43 +0200

    create fake data (faker.js doesn't work)

---
+++ src/common/components/Feed/Suggestions/data.tsx
@@ -0,0 +1,55 @@
+
+
+const data =
+  [
+    {
+      id: "1",
+      name: 'Vladimir Oblast',
+      avatar: '/assets/1.jpg',
+      company: {
+        name: "Natural Gas Ltd."
+      }
+    },
+    {
+      id: "2",
+      name: 'Jodi Duke',
+      avatar: '/assets/2.jpg',
+      company: {
+        name: "Rajesh Exports Ltd."
+      }
+    },
+    {
+      id: "3",
+      name: 'Leila Wilkes',
+      avatar: '/assets/3.jpg',
+      company: {
+        name: "Natural Gas Ltd."
+      }
+    },
+    {
+      id: "4",
+      name: 'Erica Sparks',
+      avatar: '/assets/4.jpg',
+      company: {
+        name: "Natural Gas Ltd."
+      }
+    },
+    {
+      id: "5",
+      name: 'Erica Soto',
+      avatar: '/assets/5.jpg',
+      company: {
+        name: "Rajesh Exports Ltd."
+      }
+    },
+    {
+      id: "6",
+      name: 'Erica Soto',
+      avatar: '/assets/5.jpg',
+      company: {
+        name: "Rajesh Exports Ltd."
+      }
+    },
+
+  ]
+export default data
