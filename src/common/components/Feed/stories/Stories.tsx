97e3d9f4bba090b334982e75b263a520415dc5d8
adham618 <adhamtarek291@gmail.com>
2022-01-08 18:43:05 +0200

    use name && image session

--- src/common/components/Feed/stories/Stories.tsx
+++ src/common/components/Feed/stories/Stories.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react"
 import Story from "./Story"
 import data from './data'
+import { useSession } from "next-auth/react"
 
 interface StoriesProps {
 
 }
 
 const Stories = ({ }: StoriesProps) => {
+  const { data: session } = useSession()
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thumb-black scrollbar-thin">
+      <Story img={session?.user?.image as string} username={session?.user?.name as string} />
       {data.map(profile => (
         <Story key={profile.id} img={profile.avatar} username={profile.name} />
       ))}
