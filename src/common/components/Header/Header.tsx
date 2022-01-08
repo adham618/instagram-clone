3f03b945bd1b1e2c442dd811c14d31ef2eb5675d
adham618 <adhamtarek291@gmail.com>
2022-01-08 15:13:10 +0200

    add sign in page

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import { HeartIcon, HomeIcon, MenuIcon, PaperAirplaneIcon, SearchIcon, UserGroupIcon } from '@heroicons/react/outline'
 import { useRouter } from "next/router"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 interface HeaderProps {
 
 }
 
 const Header = ({ }: HeaderProps) => {
+  const { data: session } = useSession()
   const router = useRouter()
   return (
     <header className="shadow-sm border-b sticky top-0 bg-white z-50">
@@ -34,15 +36,22 @@ const Header = ({ }: HeaderProps) => {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon onClick={() => router.push('/')} className="navBtn" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <div className="relative navBtn">
-            <PaperAirplaneIcon className="rotate-45 " />
-            <div className="absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
-          </div>
-          <UserGroupIcon className="navBtn" />
-          <HeartIcon className="navBtn" />
-          <div className="relative w-9 h-9  border rounded-full p-[1.5px]">
-            <Image className="rounded-full cursor-pointer" layout="fill" priority objectFit="contain" src={"/assets/4.jpg"} alt="profile-pic" draggable="false" />
-          </div>
+          {session ? (
+            <>
+              <div className="relative navBtn">
+                <PaperAirplaneIcon className="rotate-45 " />
+                <div className="absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+              </div>
+              <UserGroupIcon className="navBtn" />
+              <HeartIcon className="navBtn" />
+              <div className="relative w-9 h-9  border rounded-full p-[1.5px] cursor-pointer">
+                <Image src={session?.user?.image as string} onClick={() => signOut()} className="rounded-full" layout="fill" priority objectFit="contain" alt="profile-pic" draggable="false" />
+              </div>
+            </>
+          ) : (
+            <button className="text-[.8rem] md:text-sm" onClick={() => signIn()}>Sign In</button>
+          )}
+
         </div>
       </div>
     </header>
