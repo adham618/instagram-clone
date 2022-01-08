1b2894dc36323dea55da113c944b20c7af2e18b8
adham618 <adhamtarek291@gmail.com>
2022-01-08 18:41:21 +0200

    hide section when logged off

--- src/common/components/Feed/Feed.tsx
+++ src/common/components/Feed/Feed.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react"
 import MiniProfile from "./MiniProfile/MiniProfile"
 import Posts from "./posts/Posts"
 import Stories from "./stories/Stories"
@@ -8,19 +9,22 @@ interface FeedProps {
 }
 
 const Feed = ({ }: FeedProps) => {
+  const { data: session } = useSession()
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-5xl xl-grid-cols-3 xl:max-w-6xl mx-auto">
+    <main className={`grid grid-cols-1 md:grid-cols-3 md:max-w-5xl xl-grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-2xl"}`}>
       <section className="col-span-2">
-        <Stories />
+        {session && (
+          <Stories />
+        )}
         <Posts />
       </section>
-
-      <section className="hidden md:inline-grid">
+      {session && (<section className="hidden md:inline-grid">
         <div className="fixed top-20 mr-5">
           <MiniProfile />
           <Suggestions />
         </div>
       </section>
+      )}
     </main>
   )
 }
