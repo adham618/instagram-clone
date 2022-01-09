7114701615a8d1f6f29c768b59ff9427ea6dfbd2
adham618 <adhamtarek291@gmail.com>
2022-01-09 22:10:05 +0200

    implement firebase to posts

--- src/common/components/Feed/posts/Posts.tsx
+++ src/common/components/Feed/posts/Posts.tsx
@@ -1,36 +1,29 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
+import { Key, useEffect, useState } from "react"
+import { db } from "../../../../../firebase"
 import Post from "./Post"
 
-interface PostsProps {
-
-}
-
-const posts = [
-  {
-    id: "123",
-    username: "hgsj fshkhd",
-    userimg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    captions: "Subscribe And Destroy The Like Button For YT Algorithm"
-  }, {
-    id: "13",
-    username: "hgsj fshkhd",
-    userimg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    captions: "Subscribe And Destroy The Like Button For YT Algorithm"
-  }, {
-    id: "163",
-    username: "hgsj fshkhd",
-    userimg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    captions: "Subscribe And Destroy The Like Button For YT Algorithm"
-  },
-]
-
-const Posts = ({ }: PostsProps) => {
+const Posts = () => {
+  const [posts, setPosts] = useState<any>([])
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy('timeStamp', 'desc')),
+        (snapshot) => {
+          return setPosts(snapshot.docs)
+        })
+    , []
+  )
   return (
     <div>
-      {posts.map(post => (
-        <Post key={post.id} username={post.username} userimg={post.userimg} img={post.img} captions={post.captions} />
+      {posts.map((post: { id: Key | null | undefined | string; data: () => { (): any; new(): any; username: string; profileImg: string; image: string; caption: string } }) => (
+        <Post
+          key={post.id}
+          id={post.id as any}
+          username={post.data().username}
+          userimg={post.data().profileImg}
+          img={post.data().image}
+          captions={post.data().caption} />
       ))}
     </div>
   )
