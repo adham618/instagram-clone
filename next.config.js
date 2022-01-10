9180bdf0fbcde84212af7134fe7222f471214158
adham618 <adhamtarek291@gmail.com>
2022-01-10 17:14:03 +0200

    remove ignoring typscript errors

--- next.config.js
+++ next.config.js
@@ -4,11 +4,4 @@ module.exports = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 }
